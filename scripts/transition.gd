246c35b975f610c368f41275cb352ca46a3dcca9
Lise Henry <liz.henry@ouvaton.org>
2024-05-20 01:15:26 +0200

    Add transition to end screen

---
+++ scripts/transition.gd
@@ -0,0 +1,9 @@
+extends CanvasLayer
+
+@onready var animation = $AnimationPlayer
+
+func change_scene(target: String) -> void:
+	animation.play("fade_to_black")
+	await animation.animation_finished
+	get_tree().change_scene_to_file(target)
+	animation.play_backwards("fade_to_black")
